b5ae8fc9619971286b31d8a85bcf2af4f160b8b0
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-11-25 12:37:29 -0800

    First_Program

---
+++ java_intro/Java_Program/JavaProgram.java
@@ -0,0 +1,7 @@
+public class JavaProgram{
+    public static void main(String[] args) {
+        System.out.println("My name is Maksim Dauhaleu");
+        System.out.println("I am 21 years old");
+        System.out.println("My home town is Belarus,Minsk");
+    }
+}

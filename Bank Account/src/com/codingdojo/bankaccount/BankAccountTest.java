81d23239462210a41847731a8b41bd1f9520d528
MaksimDauhaleu <maksimdauhaleu@gmail.com>
2019-12-02 11:26:51 -0800

    Bank Acount++

--- Bank Account/src/com/codingdojo/bankaccount/BankAccountTest.java
+++ Bank Account/src/com/codingdojo/bankaccount/BankAccountTest.java
@@ -3,12 +3,13 @@ package com.codingdojo.bankaccount;
 public class BankAccountTest {
 
 	public static void main(String[] args) {
-		BankAccount x = new BankAccount("25hl12",25.12,125.65);
-		BankAccount y = new BankAccount("25hl12",25.12,125.65);
-		BankAccount t = new BankAccount("25hl12",25.12,125.65);
+		BankAccount user1 = new BankAccount();
+		BankAccount user2 = new BankAccount();
+		BankAccount user3 = new BankAccount();
 		
-		System.out.println(x.AccountCount());
-		System.out.println(x.Total());
-		System.out.println((25.12 * 3) + (125.65 * 3));
+		user1.makeDeposit("saving",25.00);
+		user1.makeDeposit("saving",25.00);
+		user1.makeWithdraw("saving", 10);
+		user1.printAll();
 	}
 }
